9b26951d8edaf3f555e292425a29adcee0992814
Bemnet-21 <anjejobemnet@gmail.com>
2025-12-31 22:43:44 +0300

    add model for confession

--- backend/ConfessionWall/src/main/java/com/confessionwall/model/ConfessionModel.java
+++ backend/ConfessionWall/src/main/java/com/confessionwall/model/ConfessionModel.java
@@ -1,5 +1,29 @@
 package com.confessionwall.model;
 
-public class ConfessionModel {
+import java.sql.Timestamp;
 
+public class ConfessionModel {
+	private int id;
+	private String content;
+	private int likes;
+	private int user_id;
+	private Timestamp createdAt;
+	
+	public ConfessionModel() {};
+	
+	public ConfessionModel(String content, int user_id) {
+		this.content = content;
+		this.user_id = user_id;
+	}
+	
+	public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+    public int getLikes() { return likes; }
+    public void setLikes(int likes) { this.likes = likes; }
+    public int getUserId() { return user_id; }
+    public void setUserId(int userId) { this.user_id = userId; }
+    public Timestamp getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
 }
